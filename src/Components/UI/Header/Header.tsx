f010f8429106f42171773484d7db5c5ca5d9a0db
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-01-22 07:08:09 +0500

    fix styless header, input, footer

--- src/Components/UI/Header/Header.tsx
+++ src/Components/UI/Header/Header.tsx
@@ -5,36 +5,33 @@ import s from "./Header.module.css";
 export const Header = () => {
   return (
     <header className={s.Header}>
+      <span>{atom}</span>
       <div className={s.HeaderContent}>
-        <span>{atom}</span>
-        <div className={s.HeaderContentLinks}>
-          <ul>
-            <li>
-              <a href="/"> EIR</a>
-            </li>
-            <li>
-              <a href="/">Инструкции</a>
-            </li>
-            <li>
-              <a href="/">Библиотека</a>
-            </li>
-            <li>
-              <a href="/">Видео</a>
-            </li>
-            <li>
-              <a href="/">Faq</a>
-            </li>
-          </ul>
-          <AtomInput
-            type="text"
-            value={""}
-            onChange={() => console.log("ionput")}
-            placeholder="Поиск"
-          />
-        </div>
-
-        <AtomButton onClick={() => console.log("Open")}>Войти</AtomButton>
+        <ul>
+          <li>
+            <a href="/"> EIR</a>
+          </li>
+          <li>
+            <a href="/">Инструкции</a>
+          </li>
+          <li>
+            <a href="/">Библиотека</a>
+          </li>
+          <li>
+            <a href="/">Видео</a>
+          </li>
+          <li>
+            <a href="/">Faq</a>
+          </li>
+        </ul>
       </div>
+      <AtomInput
+        type="text"
+        value={""}
+        onChange={() => console.log("ionput")}
+        placeholder="Поиск"
+      />
+      <AtomButton onClick={() => console.log("Open")}>Войти</AtomButton>
     </header>
   );
 };
