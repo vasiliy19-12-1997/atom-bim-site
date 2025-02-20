961883da809c0973e9f30e6ce100f83476a18737
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-02-20 17:03:43 +0500

    1

--- src/Components/Pages/InstructionPage.tsx
+++ src/Components/Pages/InstructionPage.tsx
@@ -12,70 +12,75 @@ export const InstructionPage = () => {
   return (
     <>
       <section className={s.InstructionPage}>
-        <aside className={s.InstructionPageLeftAside}>
-          <nav>
-            <ul>
-              <li>
-                <a href="/">СОД</a>
-              </li>
-              <li>
-                <a href="/">Revit Общие</a>
-              </li>
-              <li>
-                <a href="/">Revit АР</a>
-              </li>
-              <li>
-                <a href="/">Revit КЖ</a>
-              </li>
-              <li>
-                <a href="/">Revit ОВ/ВК</a>
-              </li>
-              <li>
-                <a href="/">Revit ЭЛ/СС</a>
-              </li>
-              <li>
-                <a href="/">Civil 3D</a>
-              </li>
-              <li>
-                <a href="/">AutoCad</a>
-              </li>
-              <li>
-                <a href="/">Tangl Value</a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <button className={s.InstructionPageLeftButton}>Показать/скрыть</button>
-        <div className={s.InstructionPageContent}>
-          <h2>Content</h2>
-          <p>{lorem}</p>
+        <div className={s.InstructionPageBlock}>
+          <aside className={s.InstructionPageLeftAside}>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">СОД</a>
+                </li>
+                <li>
+                  <a href="/">Revit Общие</a>
+                </li>
+                <li>
+                  <a href="/">Revit АР</a>
+                </li>
+                <li>
+                  <a href="/">Revit КЖ</a>
+                </li>
+                <li>
+                  <a href="/">Revit ОВ/ВК</a>
+                </li>
+                <li>
+                  <a href="/">Revit ЭЛ/СС</a>
+                </li>
+                <li>
+                  <a href="/">Civil 3D</a>
+                </li>
+                <li>
+                  <a href="/">AutoCad</a>
+                </li>
+                <li>
+                  <a href="/">Tangl Value</a>
+                </li>
+              </ul>
+            </nav>
+            <button className={s.InstructionPageLeftButton}>
+              Показать/скрыть
+            </button>
+          </aside>
+
+          <div className={s.InstructionPageContent}>
+            <h2>Content</h2>
+            <p>{lorem}</p>
+          </div>
         </div>
+        <aside className={s.RightAside}>
+          <ol>
+            <li>
+              <a href="/">Описание семейства</a>
+            </li>
+            <li>
+              <a href="/">Общие требования</a>
+            </li>
+            <li>
+              <a href="/">Правила наименования</a>
+            </li>
+            <li>
+              <a href="/">Правила маркировки</a>
+            </li>
+            <li>
+              <a href="/">Параметры типоразмера</a>
+            </li>
+            <li>
+              <a href="/">Структура слоев</a>
+            </li>
+            <li>
+              <a href="/">Параметры экземпляра</a>
+            </li>
+          </ol>
+        </aside>
       </section>
-      <aside className={s.RightAside}>
-        <ol>
-          <li>
-            <a href="/">Описание семейства</a>
-          </li>
-          <li>
-            <a href="/">Общие требования</a>
-          </li>
-          <li>
-            <a href="/">Правила наименования</a>
-          </li>
-          <li>
-            <a href="/">Правила маркировки</a>
-          </li>
-          <li>
-            <a href="/">Параметры типоразмера</a>
-          </li>
-          <li>
-            <a href="/">Структура слоев</a>
-          </li>
-          <li>
-            <a href="/">Параметры экземпляра</a>
-          </li>
-        </ol>
-      </aside>
     </>
   );
 };
