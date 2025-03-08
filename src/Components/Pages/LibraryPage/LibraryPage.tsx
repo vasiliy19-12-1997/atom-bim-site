703308d024d13968aa48b971fd8e6085527e62aa
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-03-08 14:04:51 +0500

    library page main styles

--- src/Components/Pages/LibraryPage/LibraryPage.tsx
+++ src/Components/Pages/LibraryPage/LibraryPage.tsx
@@ -1,8 +1,204 @@
 import s from "./LibraryPage.module.scss";
 export const LibraryPage = () => {
   return (
-    <>
-      <div className={s.LibraryPage}></div>
-    </>
+    <div className={s.LibraryPage}>
+      <div className={s.LibraryPageMain}>
+        <section className={s.LibraryPageMainSection}>
+          <h1>Шаблоны для Revit 2020</h1>
+          <hr />
+          <div className={s.Card}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+          </div>
+        </section>
+        <section className={s.LibraryPageMainSection}>
+          <h1>Шаблоны для Revit 2020</h1>
+          <hr />
+          <div className={s.Card}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+          </div>
+        </section>
+        <section className={s.LibraryPageMainSection}>
+          <h1>Шаблоны для Revit 2020</h1>
+          <hr />
+          <div className={s.Card}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+          </div>
+        </section>
+        <section className={s.LibraryPageMainSection}>
+          <h1>Шаблоны для Revit 2020</h1>
+          <hr />
+          <div className={s.Card}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
