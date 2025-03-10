df404b84dbfc2b27502c51c4981e94197acc9c04
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-03-10 16:43:31 +0500

    libraryPAge

--- src/Components/Pages/LibraryPage/LibraryPage.tsx
+++ src/Components/Pages/LibraryPage/LibraryPage.tsx
@@ -61,6 +61,21 @@ export const LibraryPage = () => {
               />
               <p>АСК_Шаблон_АР (Жилье)</p>
             </div>
+          </div>
+        </article>
+        <article className={s.LibraryPageMainSection}>
+          <h1>Шаблоны для Civil3D 2023</h1>
+          <hr />
+          <div className={s.Card}>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                alt="test"
+                width={150}
+                height={200}
+              />
+              <p>АСК_Шаблон_АР (Жилье)</p>
+            </div>
             <div>
               <img
                 src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
@@ -100,84 +115,9 @@ export const LibraryPage = () => {
           </div>
         </article>
         <article className={s.LibraryPageMainSection}>
-          <h1>Шаблоны для Revit 2020</h1>
+          <h1>Шаблоны для Autocad 2020-2023</h1>
           <hr />
           <div className={s.Card}>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-          </div>
-        </article>
-        <article className={s.LibraryPageMainSection}>
-          <h1>Шаблоны для Revit 2020</h1>
-          <hr />
-          <div className={s.Card}>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
-          </div>
-        </article>
-        <article className={s.LibraryPageMainSection}>
-          <h1>Шаблоны для Revit 2020</h1>
-          <hr />
-          <div className={s.Card}>
-            <div>
-              <img
-                src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                alt="test"
-                width={150}
-                height={200}
-              />
-              <p>АСК_Шаблон_АР (Жилье)</p>
-            </div>
             <div>
               <img
                 src="https://images.unsplash.com/photo-1734630378523-c6735d798820?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
