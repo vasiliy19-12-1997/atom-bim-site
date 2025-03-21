b6898fb2171357773718b9052ba20c6c828d6ea6
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-03-21 14:55:27 +0500

    instruction page fix adaptive

--- client/src/Components/Pages/InstructionPage/InstructionPage.tsx
+++ client/src/Components/Pages/InstructionPage/InstructionPage.tsx
@@ -9,79 +9,86 @@ export const InstructionPage = () => {
     setIsLeftAsideOpen(!isLeftAsideOpen);
   };
   return (
-    <section className={s.InstructionPage}>
-      <Aside
-        show={isLeftAsideOpen}
-        className={`${s.InstructionPageLeftAside} ${
-          isLeftAsideOpen ? s.open : s.close
-        }`}
-      >
-        <nav>
-          <ul>
+    <>
+      <section className={s.InstructionPage}>
+        <div className={s.InstructionPageMain}>
+          <Aside
+            show={isLeftAsideOpen}
+            className={`${s.InstructionPageMainLeftAside} ${
+              isLeftAsideOpen ? s.open : s.close
+            }`}
+          >
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">СОД</a>
+                </li>
+                <li>
+                  <a href="/">Revit Общие</a>
+                </li>
+                <li>
+                  <a href="/">Revit АР</a>
+                </li>
+                <li>
+                  <a href="/">Revit КЖ</a>
+                </li>
+                <li>
+                  <a href="/">Revit ОВ/ВК</a>
+                </li>
+                <li>
+                  <a href="/">Revit ЭЛ/СС</a>
+                </li>
+                <li>
+                  <a href="/">Civil 3D</a>
+                </li>
+                <li>
+                  <a href="/">AutoCad</a>
+                </li>
+                <li>
+                  <a href="/">Tangl Value</a>
+                </li>
+              </ul>
+            </nav>
+          </Aside>
+          <div></div>
+          <button
+            onClick={toggleLeftAside}
+            className={s.InstructionPageMainLeftButton}
+          >
+            {isLeftAsideOpen ? "◀" : "▶"}
+          </button>
+          <div className={s.InstructionPageMainContent}>
+            <h2>Content</h2>
+            <p>{lorem}</p>
+          </div>
+        </div>
+        <div></div>
+        <Aside className={s.RightAside}>
+          <ol>
             <li>
-              <a href="/">СОД</a>
+              <a href="/">Описание семейства</a>
             </li>
             <li>
-              <a href="/">Revit Общие</a>
+              <a href="/">Общие требования</a>
             </li>
             <li>
-              <a href="/">Revit АР</a>
+              <a href="/">Правила наименования</a>
             </li>
             <li>
-              <a href="/">Revit КЖ</a>
+              <a href="/">Правила маркировки</a>
             </li>
             <li>
-              <a href="/">Revit ОВ/ВК</a>
+              <a href="/">Параметры типоразмера</a>
             </li>
             <li>
-              <a href="/">Revit ЭЛ/СС</a>
+              <a href="/">Структура слоев</a>
             </li>
             <li>
-              <a href="/">Civil 3D</a>
+              <a href="/">Параметры экземпляра</a>
             </li>
-            <li>
-              <a href="/">AutoCad</a>
-            </li>
-            <li>
-              <a href="/">Tangl Value</a>
-            </li>
-          </ul>
-        </nav>
-      </Aside>
-      <button onClick={toggleLeftAside} className={s.InstructionPageLeftButton}>
-        {isLeftAsideOpen ? "◀" : "▶"}
-      </button>
-      <div></div>
-      <div className={s.InstructionPageContent}>
-        <h2>Content</h2>
-        <p>{lorem}</p>
-      </div>
-      <div></div>
-      <Aside className={s.RightAside}>
-        <ol>
-          <li>
-            <a href="/">Описание семейства</a>
-          </li>
-          <li>
-            <a href="/">Общие требования</a>
-          </li>
-          <li>
-            <a href="/">Правила наименования</a>
-          </li>
-          <li>
-            <a href="/">Правила маркировки</a>
-          </li>
-          <li>
-            <a href="/">Параметры типоразмера</a>
-          </li>
-          <li>
-            <a href="/">Структура слоев</a>
-          </li>
-          <li>
-            <a href="/">Параметры экземпляра</a>
-          </li>
-        </ol>
-      </Aside>
-    </section>
+          </ol>
+        </Aside>
+      </section>
+    </>
   );
 };
