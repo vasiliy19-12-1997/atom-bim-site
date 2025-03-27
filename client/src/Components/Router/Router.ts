483a78b8c7d8067847e51407d686f90caacc82b4
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-03-27 17:46:22 +0500

    test

--- client/src/Components/Router/Router.ts
+++ client/src/Components/Router/Router.ts
@@ -5,6 +5,7 @@ import { InstructionPage } from "../Pages/InstructionPage/InstructionPage";
 import { LibraryPage } from "../Pages/LibraryPage/LibraryPage";
 import { MainPage } from "../Pages/MainPage/MainPage";
 import { VideoPage } from "../Pages/VideoPage/VideoPage";
+import { Test } from "../Pages/Test/Test";
 import { Route } from "../Types/types";
 
 export const privateRoutes: Route[] = [
@@ -14,6 +15,10 @@ export const privateRoutes: Route[] = [
   { path: "/library", element: LibraryPage },
   { path: "/video", element: VideoPage },
   { path: "/faq", element: FAQPage },
+  { path: "/test", element: Test },
 ];
 
-export const publicRoutes: Route[] = [{ path: "/login", element: AuthPage }];
+export const publicRoutes: Route[] = [
+  { path: "/login", element: AuthPage },
+  { path: "/test", element: Test },
+];
