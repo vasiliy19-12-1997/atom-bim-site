0ea747d70d018baadff5e95bc399e5dd25bb8c33
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-03-31 17:42:37 +0500

    add vercel config to server

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,12 +10,7 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin:
-      "https://atom-bim-site-client-rjhjd34i6-vasiliy19121997s-projects.vercel.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
@@ -29,5 +24,3 @@ app.use("/api/eir", eirRoutes);
 app.listen(PORT, () => {
   console.log(`Сервер запущен на :https://localhost:${PORT}`);
 });
-// Для Vercel Serverless
-module.exports = app;
