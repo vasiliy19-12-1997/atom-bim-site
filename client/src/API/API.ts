61cd5b848ccaa97a685f9487cd21bad446da8527
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-04-01 16:15:12 +0500

    try to add server to vercel

--- client/src/API/API.ts
+++ client/src/API/API.ts
@@ -4,25 +4,22 @@ import {
   IVideoSection,
 } from "../Components/Types/types";
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export const fetchVideos = async (): Promise<IVideoSection[]> => {
-  const response = await fetch(
-    "http://atom-bim-site-server.vercel.app/api/videos"
-  );
+  const response = await fetch(`${API_BASE_URL}/videos`);
   if (!response.ok) throw new Error("Ошибка загрузки данных");
   return response.json();
 };
 
 export const fetchFaq = async (): Promise<IFAQItem[]> => {
-  const response = await fetch(
-    "http://atom-bim-site-server.vercel.app//api/faq"
-  );
+  const response = await fetch(`${API_BASE_URL}/faq`);
   if (!response.ok) throw new Error("Ошибка на сервере");
   return response.json();
 };
+
 export const fetchLibrary = async (): Promise<ILibraryArray[]> => {
-  const response = await fetch(
-    "http://atom-bim-site-server.vercel.app/api/library"
-  );
+  const response = await fetch(`${API_BASE_URL}/library`);
   if (!response.ok) throw new Error("Ошибка на сервере");
   return response.json();
 };
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import videoRoutes from "./Routes/videoRoutes.js";
@@ -7,16 +10,30 @@ import eirRoutes from "./Routes/eirRoutes.js";
 import fileUpload from "express-fileupload";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Список разрешённых Origin
+const allowedOrigins = [
+  "http://localhost:3000", // локальный клиент (если используется порт 3000)
+  "https://atom-bim-site-client.vercel.app/", // продакшен домен клиента
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      // Если нет origin (например, для curl или postman) — разрешаем
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Not allowed by CORS: ${origin}`));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
@@ -28,5 +45,5 @@ app.use("/api/library", libraryRoutes);
 app.use("/api/eir", eirRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на :https://localhost:${PORT}`);
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
