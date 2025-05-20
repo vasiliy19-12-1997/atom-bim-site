28c19fd03f344feec4ccccb89d20aa2ebc14e178
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-05-20 16:49:49 +0500

    add base vervel config

--- server/src/Controllers/libraryController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { atomLibraryArray } from "../Data/libraryArray.js";
-export const getLibrary = (req: Request, res: Response) => {
-  try {
-    res.status(200).json(atomLibraryArray);
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
--- server/src/Routes/videoRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getVideos } from "../Controllers/videoController.js";
-
-const router = Router();
-
-router.get("/", getVideos);
-export default router;
