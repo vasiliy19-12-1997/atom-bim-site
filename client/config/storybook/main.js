7aff44b747546919dfcd899e8fca7e2948cd8428
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-07-02 14:00:38 +0500

    add config

---
+++ client/config/storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+    stories: [
+        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: 'webpack5',
+    },
+    server: {
+        host: 'localhost', // или '127.0.0.1'
+        port: 6006,
+    },
+};
