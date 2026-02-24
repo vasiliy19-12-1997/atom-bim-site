fa77f3e05c07282fbfd17d42c89110df02721dea
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2026-02-24 22:29:33 +0500

    add images for sections

--- src/pages/MainPage/ui/MainPageSections/MainPageSections.tsx
+++ src/pages/MainPage/ui/MainPageSections/MainPageSections.tsx
@@ -4,8 +4,8 @@ import cls from './MainPageSections.module.scss';
 import { Card } from '@/shared/ui/redesigned/Card';
 import { HStack } from '@/shared/ui/redesigned/Stack';
 import { Text } from '@/shared/ui/redesigned/Text';
-import sectionEir from '../assets/section_eir.png';
 import { AppImage } from '@/shared/ui/redesigned/AppImage';
+import { sectionsArrText } from '../../lib/const/sections';
 
 interface MainPageSectionsProps {
     className?: string;
@@ -15,39 +15,12 @@ export const MainPageSections = memo((props: MainPageSectionsProps) => {
     const { t } = useTranslation();
     const { className } = props;
 
-    const sectionsArrText = [
-        {
-            name: 'EIR',
-            text: 'Корпоративный стандарт цифрового моделирования объектов капитального строительства.',
-            img: sectionEir,
-        },
-        {
-            name: 'Библиотека',
-            text: 'Шаблоны проектов для разделоа АР, КЖ, ВК, ОВ, ЭЛ, разработанные семейства. плагины по автоматизации проектирования',
-        },
-        {
-            name: 'Этапы моделирования',
-            text: 'Поэтапное описание процесса создания цифровой модели.',
-        },
-        {
-            name: 'Инструкции',
-            text: 'Перечень инструкций и методических материалов по работе в Autodesk Revit и Civil 3D.',
-        },
-        {
-            name: 'Видеоматериалы',
-            text: 'Видеоролики, инструкции, вебинары по разработке цифровой модели.',
-        },
-        {
-            name: 'Тесты',
-            text: 'Внутренняя система тестирования сотрудников и специалистов по цифровому моделированию',
-        },
-    ];
     return (
         <HStack wrap="wrap">
             {sectionsArrText.map((section) => (
                 <Card key={section.name} className={cls.card}>
                     <Text title={section.name} text={section.text} />
-                    <AppImage src={section.img} />
+                    <AppImage src={section.img} className={cls.image} />
                 </Card>
             ))}
         </HStack>
