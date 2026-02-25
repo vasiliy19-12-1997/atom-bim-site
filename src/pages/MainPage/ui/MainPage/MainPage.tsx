d5bca3051e62873a3827ac33d06697d1c152091c
Коновалов Василий Сергеевич <konovalov@atomsk.ru>
2026-02-25 17:09:00 +0500

    add video to mainpage

--- src/pages/MainPage/ui/MainPage/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage/MainPage.tsx
@@ -4,6 +4,9 @@ import { Page } from '@/shared/ui/redesigned/Page';
 import { MainPageHeader } from '../MainPageHeader/MainPageHeader';
 import { MainPageSections } from '../MainPageSections/MainPageSections';
 import cls from './MainPage.module.scss';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { Text } from '@/shared/ui/redesigned/Text';
 
 const MainPage = memo(() => {
     const { t } = useTranslation();
@@ -12,6 +15,19 @@ const MainPage = memo(() => {
         <Page className={cls.MainPage} data-testid="MainPage">
             <MainPageHeader />
             <MainPageSections />
+            <Card className={cls.videoSection}>
+                <VStack>
+                    <Text title={t('видео о ТИМ департаменте')} />
+                    <div className="videoWrapper">
+                        <iframe
+                            // eslint-disable-next-line max-len
+                            src="https://rutube.ru/play/embed/b191fbe1f8ad0f6e3733401faba62c1c"
+                            title="Тим департмамент"
+                            allow="autoplay"
+                        />
+                    </div>
+                </VStack>
+            </Card>
         </Page>
     );
 });
