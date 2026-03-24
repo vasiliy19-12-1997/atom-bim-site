8f6145e53e2d5b7bfeb7823b51869317f09110a9
Коновалов Василий Сергеевич <konovalov@atomsk.ru>
2026-03-24 17:01:01 +0500

    style: add html and css for library page

--- src/pages/LibraryPage/ui/LibraryPage/LibraryPage.tsx
+++ src/pages/LibraryPage/ui/LibraryPage/LibraryPage.tsx
@@ -2,6 +2,11 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './LibraryPage.module.scss';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { ArticlePageTabs } from '@/features/ArticlePageTabs';
+import { ArticleType } from '@/entities/Article';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { AppImage } from '@/shared/ui/redesigned/AppImage';
 
 interface LibraryPageProps {
     className?: string;
@@ -11,6 +16,81 @@ const LibraryPage = memo((props: LibraryPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
 
-    return <div className={classNames(cls.LibraryPage, {}, [className])}>{t('LibraryPage')}</div>;
+    return (
+        <div className={classNames(cls.LibraryPage, {}, [className])}>
+            <header className={cls.header}>
+                <Text title={t('Library Page')} />
+            </header>
+            <ArticlePageTabs
+                value={ArticleType.ALL}
+                // eslint-disable-next-line react/jsx-no-bind
+                onChangeType={function (value: ArticleType): void {
+                    throw new Error('Function not implemented.');
+                }}
+            />
+            <section className={cls.Templates}>
+                <Text
+                    size="m"
+                    title={t('Шаблоны для Revit')}
+                />
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+                <Card className={cls.templateCard}>
+                    <AppImage />
+                    <Text
+                        bold
+                        size="s"
+                        text={t('Аск_Шаблон_АР')}
+                    />
+                </Card>
+            </section>
+        </div>
+    );
 });
 export default LibraryPage;
